8824a9cffd5cd4505c64fea3a79502642e568105
Brian Wu <brianwu@unr.edu>
2025-02-24 17:07:31 +0000

    fix log out finction?

--- toolbox/auth/routes.py
+++ toolbox/auth/routes.py
@@ -63,7 +63,7 @@ def logout():
 		+ "/v2/logout?"
 		+ urlencode(
 			{
-				"returnTo": url_for("home", _external=True),
+				"returnTo": url_for("home_bp.home", _external=True),
 				"client_id": os.environ.get("AUTH0_CLIENT_ID"),
 			},
 			quote_via=quote_plus,
