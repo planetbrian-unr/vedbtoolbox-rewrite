3803d9e94ac01acace546f8ffeca1399642ed268
Brian Wu <brianwu@unr.edu>
2025-03-18 18:16:32 +0000

    separate methods from user authentication. made the admin dashboard "kickstartable" without manual intervention

---
+++ toolbox/auth/methods.py
@@ -0,0 +1,45 @@
+from toolbox import db
+from toolbox.models import User
+from sqlalchemy.exc import SQLAlchemyError
+
+def first_user() -> bool:
+    # Only the first user is admin by default
+    # Allows kickstarting without need to modify SQLite DB directly.
+    return User.query.first() is None
+
+def add_user_to_local_db(token):
+    try:
+        # Parse returned JSON in token for specific attributes
+        user_info = token.get("userinfo", {})
+        user_id = user_info.get("sub")
+        username = user_info.get("nickname")
+        email = user_info.get("email")
+
+        # Add/update the user in the database
+        user_in_db = User.query.filter_by(user_id=user_id).first()
+        if not user_in_db:
+            # Add new user if not already in the database
+            new_user = User(
+                user_id=user_id,
+                username=username,
+                email=email,
+                admin=first_user()
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            return {"status": "success", "message": "User successfully added to DB."}
+        else:
+            # Update existing user if necessary
+            user_in_db.username = username
+            user_in_db.email = email
+            db.session.commit()
+            return {"status": "success", "message": "User successfully updated with new details."}
+
+    except SQLAlchemyError as e:
+        # Handle database-related errors
+        db.session.rollback()
+        return {"status": "error", "message": "An error occurred while interacting with the database."}
+
+    except Exception as e:
+        # Handle other errors
+        return {"status": "error", "message": "An unexpected error occurred."}
--- toolbox/auth/routes.py
+++ toolbox/auth/routes.py
@@ -8,47 +8,15 @@ from urllib.parse import quote_plus, urlencode
 from flask import url_for, session, redirect
 
 # local
-from toolbox import db
-from toolbox.models import User
 from toolbox.auth import blueprint
 from toolbox.auth.oauth import oauth
+from toolbox.auth.methods import add_user_to_local_db
 
 @blueprint.route("/login")
 def login():
 	return oauth.auth0.authorize_redirect(
 		redirect_uri = url_for("auth.callback", _external=True)
 	)
-
-def add_user_to_local_db(token):
-	# parse returned json in token for specific attributes
-	user_info = token.get("userinfo", {})
-	json_user_id = user_info.get("sub")
-	json_username = user_info.get("nickname")
-	json_email = user_info.get("email")
-	
-	# add/update the user in the database
-	user_in_db = User.query.filter_by(user_id=json_user_id).first()
-	if not user_in_db:
-		# Add new user if not already in the database
-		user_in_db = User(
-			user_id = json_user_id,
-			username = json_username,
-			email = json_email,
-		)
-		db.session.add(user_in_db)
-	else:
-		# Update existing user if necessary
-		user_in_db.username = json_username
-		user_in_db.email = json_email
-	db.session.commit()
-	
-	# check if user is in system
-	user_in_db = User.query.filter_by(user_id=user_info.get("sub")).first()
-	if user_in_db:
-		return {"status": "success", "message": "User successfully added or updated."}
-	else:
-		return {"status": "failure", "message": "User could not be added or updated."}
-
 
 # finalizing authentication
 @blueprint.route("/callback")
@@ -69,11 +37,12 @@ def logout():
 	session.clear()
 	
 	return redirect(
-		"https://" + env.get("AUTH0_DOMAIN") + "/v2/logout?" + urlencode(
+		"https://" + env.get("AUTH0_DOMAIN") + "/v2/logout?" + 
+		urlencode(
 			{
 				"returnTo": url_for("home.home", _external=True),
 				"client_id": env.get("AUTH0_CLIENT_ID"),
 			},
-			quote_via=quote_plus,
+			quote_via = quote_plus,
 		)
 	)
