32321c4c7d6574ea366efd1ce69f4576d5c82301
Brian Wu <brianwu@unr.edu>
2025-03-21 18:57:42 +0000

    i actually hate git

--- toolbox/home/methods.py
+++
@@ -1,9 +0,0 @@
-# flask
-from flask import session, redirect
-
-def check():
-    # If the session is available AKA the user is logged in, redirect to file_upload
-    if session:
-        return redirect("/file_upload")
-    # else do nothing
-    return None
